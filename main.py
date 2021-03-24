19b6d99d9db62d6af5161c05c57c105c33585949
mike56k <isachenko.mikhail.k@gmail.com>
2021-03-24 21:20:46 +0300

    sdffsfj

--- main.py
+++ main.py
@@ -125,6 +125,53 @@ app.add_middleware(
 )
 
 
+@app.exception_handler(500)
+async def custom_http_exception_handler(request, exc):
+    error = ErrorResponse(error="Something went wrong")
+    error = jsonable_encoder(error.dict())
+
+    response = JSONResponse(content=error, status_code=500)
+
+    # Since the CORSMiddleware is not executed when an unhandled server exception
+    # occurs, we need to manually set the CORS headers ourselves if we want the FE
+    # to receive a proper JSON 500, opposed to a CORS error.
+    # Setting CORS headers on server errors is a bit of a philosophical topic of
+    # discussion in many frameworks, and it is currently not handled in FastAPI.
+    # See dotnet core for a recent discussion, where ultimately it was
+    # decided to return CORS headers on server failures:
+    # https://github.com/dotnet/aspnetcore/issues/2378
+    origin = request.headers.get('origin')
+
+    if origin:
+        # Have the middleware do the heavy lifting for us to parse
+        # all the config, then update our response headers
+        cors = CORSMiddleware(
+            app=app,
+            allow_origins=ALLOWED_ORIGINS,
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"])
+
+        # Logic directly from Starlette's CORSMiddleware:
+        # https://github.com/encode/starlette/blob/master/starlette/middleware/cors.py#L152
+
+        response.headers.update(cors.simple_headers)
+        has_cookie = "cookie" in request.headers
+
+        # If request includes any cookie headers, then we must respond
+        # with the specific origin instead of '*'.
+        if cors.allow_all_origins and has_cookie:
+            response.headers["Access-Control-Allow-Origin"] = origin
+
+        # If we only allow specific origins, then we have to mirror back
+        # the Origin header in the response.
+        elif not cors.allow_all_origins and cors.is_allowed_origin(origin=origin):
+            response.headers["Access-Control-Allow-Origin"] = origin
+            response.headers.add_vary_header("Origin")
+
+    return response
+
+
 @app.on_event("startup")
 async def startup():
     await database.connect()
