2b612c39c17cc1e60af2c41fb024186b77844c96
mike56k <isachenko.mikhail.k@gmail.com>
2021-03-24 21:13:18 +0300

    sdffsf

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
 
