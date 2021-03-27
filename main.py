7742571cd327c8b360c9319f791af6bf49f46733
Данила Комлев <m1902452@edu.misis.ru>
2021-03-27 13:05:23 +0300

    ghfd

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
