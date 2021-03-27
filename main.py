c681b0818e605528ea40b2c2da98a9fd52084dd5
Данила Комлев <m1902452@edu.misis.ru>
2021-03-27 13:00:15 +0300

    ghd

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
